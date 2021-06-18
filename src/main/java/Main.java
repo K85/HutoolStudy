4b485e0bc957ab5bdedc2e2ae3e076a5f1a5eae2
HakuzosuSAMA <3172906506@qq.com>
2021-06-18 22:43:29 +0800

    FIRST COMMIT

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import cn.hutool.core.convert.Convert;
+import cn.hutool.log.Log;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a;
+        Log.get().info("左边{}右边", "2");
+
+
+
+    }
+
+
+}
